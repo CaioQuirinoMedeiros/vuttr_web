6d4ee4a83dff745cecad2cc4ed8220bdfa66b1fb
Caio Quirino Medeiros <caio.quirino.medeiros@gmail.com>
2019-07-25 18:49:47 -0300

    Structuring App: routes, globalstyle and toast container

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { ThemeProvider } from "styled-components";
+import { ToastContainer, Flip } from "react-toastify";
+
+import Routes from "./routes";
+
+import GlobalStyle from "./styles/global";
+import theme from "./styles/theme";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <>
+        <Routes />
+        <GlobalStyle />
+        <ToastContainer transition={Flip} autoClose={3500} hideProgressBar />
+      </>
+    </ThemeProvider>
   );
 }
 
