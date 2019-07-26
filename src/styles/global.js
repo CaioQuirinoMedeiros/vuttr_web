3fe7d46e2da4a0660b9ba0a1fbb1a149de5f8841
Caio Quirino Medeiros <caio.quirino.medeiros@gmail.com>
2019-07-26 04:29:36 -0300

    Deleting unused colors and renaming others

--- src/styles/global.js
+++ src/styles/global.js
@@ -15,27 +15,14 @@ export default createGlobalStyle`
     font-size: 62.5%;
     --color-white: ${props => props.theme.colors.white};
     --color-darkWhite: ${props => props.theme.colors.darkWhite};
-    --color-darkerWhite: ${props => props.theme.colors.darkerWhite};
-    --color-darkestWhite: ${props => props.theme.colors.darkestWhite};
-    --color-mostDarkWhite: ${props => props.theme.colors.mostDarkWhite};
     --color-ink: ${props => props.theme.colors.Ink};
     --color-lightInk: ${props => props.theme.colors.lightInk};
-    --color-lighterInk: ${props => props.theme.colors.lighterInk};
-    --color-lightestInk: ${props => props.theme.colors.lightestInk};
-    --color-darkerBlue: ${props => props.theme.colors.darkerBlue};
-    --color-darkBlue: ${props => props.theme.colors.darkBlue};
-    --color-blue: ${props => props.theme.colors.blue};
-    --color-lightBlue: ${props => props.theme.colors.lightBlue};
-    --color-lighterBlue: ${props => props.theme.colors.lighterBlue};
-    --color-lightestBlue: ${props => props.theme.colors.lightestBlue};
-    --color-mostLightestBlue: ${props => props.theme.colors.mostLightestBlue};
-    --color-darkerRed: ${props => props.theme.colors.darkerRed};
-    --color-darkRed: ${props => props.theme.colors.darkRed};
-    --color-red: ${props => props.theme.colors.red};
-    --color-ligtRed: ${props => props.theme.colors.ligtRed};
-    --color-lighterRed: ${props => props.theme.colors.lighterRed};
-    --color-lightestRed: ${props => props.theme.colors.lightestRed};
-    --color-mostLightestRed: ${props => props.theme.colors.mostLightestRed};
+    --color-primary: ${props => props.theme.colors.primary};
+    --color-primaryHover: ${props => props.theme.colors.primaryHover};
+    --color-danger: ${props => props.theme.colors.danger};
+    --color-dangerHover: ${props => props.theme.colors.dangerHover};
+    --color-success: ${props => props.theme.colors.success};
+    --color-successHover: ${props => props.theme.colors.successHover};
     --color-darkerShadow: ${props => props.theme.colors.darkerShadow};
     --color-darkShadow: ${props => props.theme.colors.darkShadow};
     --color-shadow: ${props => props.theme.colors.shadow};
@@ -59,7 +46,7 @@ export default createGlobalStyle`
     display: flex;
     flex-direction: column;
     background: var(--color-darkWhite);
-    color: var(--color-Ink);
+    color: var(--color-ink);
     font-size: 1.4rem;
     font-family: 'Source Sans Pro', "Arial", sans-serif;
     text-rendering: optimizeLegibility !important;
@@ -124,5 +111,23 @@ export default createGlobalStyle`
   a {
     text-decoration: none;
     color: inherit;
+  }
+
+  .toast-success {
+    border-radius: 3px !important;
+    background: var(--color-success) !important; 
+  
+    &:hover {
+      background: var(--color-successHover);
+    }
+  }
+
+  .toast-error {
+    border-radius: 3px !important;
+    background: var(--color-danger) !important; 
+  
+    &:hover {
+      background: var(--color-dangerHover);
+    }
   }
 `;
