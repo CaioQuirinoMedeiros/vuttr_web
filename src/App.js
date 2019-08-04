c5873c5d39ce6e64a3be19d012bcc142b877731e
Caio Quirino Medeiros <caio.quirino.medeiros@gmail.com>
2019-08-04 18:42:34 -0300

    some fixes

--- src/App.js
+++ src/App.js
@@ -7,16 +7,14 @@ import Routes from "./routes"
 import GlobalStyle from "./styles/global"
 import theme from "./styles/theme"
 
-function App() {
-  return (
-    <ThemeProvider theme={theme}>
-      <>
-        <Routes />
-        <GlobalStyle />
-        <ToastContainer transition={Flip} autoClose={3500} hideProgressBar />
-      </>
-    </ThemeProvider>
-  )
-}
+const App = () => (
+  <ThemeProvider theme={theme}>
+    <>
+      <Routes />
+      <GlobalStyle />
+      <ToastContainer transition={Flip} autoClose={3500} hideProgressBar />
+    </>
+  </ThemeProvider>
+)
 
 export default App
